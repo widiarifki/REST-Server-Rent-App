7ce9dda7e2c0312ea247673b3b53b08be4a3613f
widiarifki <widiarifki@gmail.com>
2016-06-16 22:43:38 +0700

    update MemberServlet.java

--- src/main/java/com/widiarifki/rental/servlet/MemberServlet.java
+++ src/main/java/com/widiarifki/rental/servlet/MemberServlet.java
@@ -16,7 +16,7 @@ public class MemberServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 	}
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		try{
