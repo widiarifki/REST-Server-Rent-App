6147dae834756d250ead5a345322ef021bc1cef0
widiarifki <widiarifki@gmail.com>
2016-06-17 03:06:11 +0700

    modify MemberServlet

--- src/main/java/com/widiarifki/rental/servlet/MemberServlet.java
+++ src/main/java/com/widiarifki/rental/servlet/MemberServlet.java
@@ -2,13 +2,11 @@ package com.widiarifki.rental.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.sql.SQLException;
-import java.util.Formatter;
 import java.util.Date;
 import java.util.ArrayList;
 import javax.servlet.http.HttpServlet;
@@ -24,7 +22,7 @@ public class MemberServlet extends HttpServlet {
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {		
-		String sql = "INSERT INTO members (name, email, phone, password, date_register) "
+		String sql = "INSERT INTO members (name, email, phone, password, date_register, status) "
 			+" VALUES (?, ?, ?, ?, ?)";
 
 		Connection con = DBConnectionHandler.getConnection();
@@ -35,26 +33,24 @@ public class MemberServlet extends HttpServlet {
 			String phone = req.getParameter("phone");
 			String password = req.getParameter("password");
 
-			//MessageDigest sha1 = MessageDigest.getInstance("SHA1");
-
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setString(1, name);
 			ps.setString(2, email);
 			ps.setString(3, phone);
-			//ps.setString(4, calculateHash(sha1, email));
 			ps.setString(4, DigestUtils.sha1Hex(password));
 			ps.setTimestamp(5, getCurrentTimeStamp());
+			ps.setInt(6, 1);
 
 			ps.executeUpdate();
 
 			res.setContentType("application/json");
-		res.getWriter().println(sql);
-		res.getWriter().flush();
+			res.getWriter().println(sql);
+			res.getWriter().flush();
 
         } catch (SQLException e) {
-        		res.setContentType("application/json");
-		res.getWriter().println(e.getMessage());
-		res.getWriter().flush();
+        	res.setContentType("application/json");
+			res.getWriter().println(e.getMessage());
+			res.getWriter().flush();
         }
 
         
@@ -64,18 +60,4 @@ public class MemberServlet extends HttpServlet {
 		Date today = new Date();
 		return new Timestamp(today.getTime());
 	}
-
-	public static String calculateHash(MessageDigest algorithm, String message) throws Exception{
-		algorithm.update(message.getBytes());
-		byte[] hash = algorithm.digest();
-		return byteArray2Hex(hash);
-    }
-
-    private static String byteArray2Hex(byte[] hash) {
-        Formatter formatter = new Formatter();
-        for (byte b : hash) {
-            formatter.format("%02x", b);
-        }
-        return formatter.toString();
-    }
 }
